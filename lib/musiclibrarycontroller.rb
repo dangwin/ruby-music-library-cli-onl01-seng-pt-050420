61366027fde9a6891a7c5980da3dc6b3b9ac537d
Dang Nguyen <danghwin@gmail.com>
2020-06-13 13:53:46 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -45,7 +45,10 @@ class MusicLibraryController
 end 
  
     def list_songs
-  Song.
+   Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
+      puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
+    
 
 
 
