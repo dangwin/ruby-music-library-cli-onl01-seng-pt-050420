1664d08561dc10768ebf4eab508e57bbdba1daf6
Dang Nguyen <danghwin@gmail.com>
2020-06-13 13:47:44 +0000

    Automatically backed up by Learn

---
+++ lib/musiclibrarycontroller.rb
@@ -0,0 +1,18 @@
+class MusicLibraryController
+  
+  attr_accessor :path
+  
+ def initialize(path = './db/mp3s')
+    importer = MusicImporter.new(path)
+    importer.import
+  end
+  
+  def call 
+    input = ''
+    
+    while input != 'exit'
+    puts "Welcome to your music library!"
+  end 
+end 
+  
+end 
